67724d8d24c46aa932646ff040db86f8ad5034ec
alcinacarlos <calcrom0607@g.educaand.es>
2024-09-26 19:44:27 +0200

    terminado

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,21 @@
-import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 import java.nio.file.Path
-import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
 
 fun convertToDouble(value: String): Double {
     // Eliminar puntos que separan miles y reemplazar la coma decimal por un punto
     val cleanedValue = value.replace(".", "").replace(",", ".")
     return cleanedValue.toDouble()
 }
-
+fun isNum(input: String): Boolean {
+    val numero = input.replace(".", "").replace(",", ".")
+    return when{
+        numero.toIntOrNull() != null -> true
+        numero.toDoubleOrNull() != null -> true
+        numero.toLongOrNull() != null -> true
+        else -> false
+    }
+}
 fun CSVToColumns(filePath: Path): Map<String, List<String>>{
     val br = Files.newBufferedReader(filePath)
     val rows = mutableListOf<List<String>>()
@@ -29,8 +36,26 @@ fun CSVToColumns(filePath: Path): Map<String, List<String>>{
     }
     return column
 }
-fun statsToCSV(data: Map<String, List<String>>, outputFilePath: Path) {
+fun statsToCSV(data: Map<String, List<String>>, path: Path) {
+    Files.write(path, "Columna;Mínimo;Máximo;Media\n".toByteArray(), StandardOpenOption.CREATE, StandardOpenOption.APPEND)
 
+    for ((key, value) in data) {
+        var lineToWrite = ""
+        val valueDouble = mutableListOf<Double>()
+        value.forEach { item ->
+            if (isNum(item)){
+                valueDouble.add(convertToDouble(item))
+            }
+        }
+        if (valueDouble.size > 0){
+            val min = valueDouble.min()
+            val max = valueDouble.max()
+            val average = valueDouble.average()
+            lineToWrite = "${key};${min};${max};${"%.2f".format(average)}\n"
+            Files.write(path, lineToWrite.toByteArray(), StandardOpenOption.APPEND)
+        }
+
+    }
 }
 
 fun main() {
@@ -38,4 +63,5 @@ fun main() {
     val outputFilePath = Path.of("src","main","resources","output.csv")
 
     val columnData = CSVToColumns(filePath)
+    statsToCSV(columnData, outputFilePath)
 }
